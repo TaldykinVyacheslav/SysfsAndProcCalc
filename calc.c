51a7dd3b1a9f76b7e7b24e819a24a6616ff2e74f
Slava <zegoline@gmail.com>
2014-04-09 00:26:14 +0300

    added calculate function

--- calc.c
+++ calc.c
@@ -13,25 +13,43 @@
 #include <linux/proc_fs.h>
 #endif
 
-#define WRITE_SIZE 100
-
+// names
 #define ARG1 "arg1"
 #define ARG2 "arg2"
 #define OPERATION "operation"
 #define RESULT "result"
 
 #define PARENT_DIR "calc"
-
-struct proc_dir_entry *calc_dir;
-struct proc_dir_entry *arg1;
-struct proc_dir_entry *arg2;
-struct proc_dir_entry *operation;
-struct proc_dir_entry *result;
-
+#define WRITE_SIZE 100
 
 static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
+
+
+long calculate(void) {
+	long a1 = 0;
+	long a2 = 0;
+	long res = 0;
+
+	if (arg1_input[strlen(arg1_input) - 2] == '\n') {
+		arg1_input[strlen(arg1_input) - 2] = (char)0;
+	}
+
+	kstrtol(arg1_input, 10, &a1);
+	kstrtol(arg2_input, 10, &a2);
+
+	if (operation_input[0] == '+') {
+		res = a1 + a2;
+	} else if (operation_input[0] == '-') {
+		res = a1 - a2;
+	} else if (operation_input[0] == '.') {
+		res = a1 * a2;
+	} else if (operation_input[0] == '/') {
+		res = a1 / a2;
+	}
+	return res;
+}
 
 /*
  * arg1 write handler
